fb1124dedd6f02fab1243d827edd209f1309ac08
omgkotofey <omgkotofey@gmail.com>
2019-08-14 01:55:46 +0300

    Class CampaignService now have methods to manipulate with user's campaigns.

--- app/domain/CampaignService.php
+++ app/domain/CampaignService.php
@@ -4,7 +4,51 @@
 namespace app\domain;
 
 
+use app\core\CatBotDB;
+use Longman\TelegramBot\Exception\TelegramException;
+
 class CampaignService
 {
+	
+	/**
+	 * @param $user_id
+	 *
+	 * @return bool
+	 * @throws TelegramException
+	 */
+	public function createNewUserCampaign($user_id)
+	{
+		if (!$this->isUserHaveAlreadyStartedCampaign($user_id)){
+			return CatBotDB::insertCampaign($user_id);
+		}
+		return true;
+	}
+	
+	/**
+	 * @param $user_id
+	 *
+	 * @return bool
+	 * @throws TelegramException
+	 */
+	public function getUserCampaign($user_id)
+	{
+		return CatBotDB::selectCampaign($user_id);
+	}
+	
+	/**
+	 * @param $user_id
+	 *
+	 * @return bool
+	 * @throws TelegramException
+	 */
+	public function isUserHaveAlreadyStartedCampaign($user_id)
+	{
+		$campaigns = $this->getUserCampaign($user_id);
+		if (is_array($campaigns)){
+			return count($campaigns) > 0;
+		}
+		return false;
+	}
+	
 	
 }
