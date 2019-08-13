a72950df5125788eb2b7a80547eb206084e8dced
omgkotofey <omgkotofey@gmail.com>
2019-08-13 23:28:59 +0300

    Front controller added.

---
+++ public_html/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Front controller
+ *
+ */
+
+require_once '../vendor/autoload.php';
+
+define('APPROOT_DIR', dirname(__DIR__));
+
+$localConfig = [];
+
+if (file_exists(APPROOT_DIR . '/config.php')) {
+	$localConfig = require(APPROOT_DIR . '/config.php');
+}
+else {
+	die('config.php is missing.');
+}
+
+require(APPROOT_DIR . '/vendor/autoload.php');
+
+$config = array_merge(
+	require(APPROOT_DIR . '/app/config/bot.php'),
+	$localConfig
+);
+
+$bot = \app\core\Bot::create($config);
+$bot->run();
