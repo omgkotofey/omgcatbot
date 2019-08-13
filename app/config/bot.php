45e55f337d6bdd40d91e6aff9a40bb2de9da13d5
omgkotofey <omgkotofey@gmail.com>
2019-08-13 23:18:48 +0300

    Application sample configuration and bot's common configuration added

---
+++ app/config/bot.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'commands_paths' => [
+		dirname(__DIR__) . '/commands',
+	],
+	'logs_path' => dirname(__DIR__) . '/logs',
+];
---
+++ config-sample.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+	/**
+	 * Bot's API token
+	 */
+	'bot_token' => '111111111:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+	/**
+	 * Bot's username without @ symbol
+	 */
+	'bot_username' => 'example_bot',
+	/**
+	 * Array of users which will have admin access to bot's private chat
+	 */
+	'bot_admins' =>[
+		000000000
+	],
+	/**
+	 * Webhook url. Not needed if you will use any of getUpdates() method
+	 */
+	'webhook_url' => 'https://example.com/',
+	/**
+	 * MySQL database credentials. Always necessary.
+	 */
+	'db' => [
+		'host'     => 'localhost',
+		'port'     => 3306,
+		'user'     => 'example_user',
+		'password' => 'example_password',
+		'database' => 'example_database',
+	],
+	/**
+	 * Enable or disable logs (/app/logs dir needs to be at least 755 accesses)
+	 */
+	'enable_logs' => false
+];
