aafab2a2ad3c0df2dc201506413e66c06ba0ea31
omgkotofey <omgkotofey@gmail.com>
2019-08-14 01:56:32 +0300

    '/startcampaign' bot command class changed. Now it's use database to store changes with campaigns.

--- app/commands/StartCampaignCommand.php
+++ app/commands/StartCampaignCommand.php
@@ -4,12 +4,14 @@
 namespace Longman\TelegramBot\Commands\UserCommands;
 
 
-use app\core\Bot;
+use app\core\CatBot;
+use app\utils\BotDevelopmentHelper;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\InlineKeyboard;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request;
+use Longman\TelegramBot\ChatAction;
 
 class StartCampaignCommand extends UserCommand
 {
@@ -39,21 +41,47 @@ class StartCampaignCommand extends UserCommand
 	{
 		$message = $this->getMessage();
 		$chat_id = $message->getChat()->getId();
-		$user_id = $message->getChat()->getUsername();
+		$user_id = $message->getFrom()->getId();
 		
-		$keyboard = new InlineKeyboard([
-			['text' => 'Follow our Twitter', 'url' => Bot::app()->config->get('twitter_profile_to_follow_url')]
+		Request::sendChatAction([
+			'chat_id' => $chat_id,
+			'action'  => ChatAction::TYPING,
 		]);
-		$keyboard->setResizeKeyboard(true);
-		$keyboard->setOneTimeKeyboard(true);
 		
-		$text    =  'Follow us on Twitter, and retweet last post - Paste link to tweet below:';
+		$text = '';
+		
+		if (CatBot::app()->campaignService->isUserHaveAlreadyStartedCampaign($user_id)){
+			$text .= "I think you already started a company. Don't piss me off pls." . PHP_EOL  . PHP_EOL;
+		}
+		
+		$campaignStarted = CatBot::app()->campaignService->createNewUserCampaign($user_id);
+		
+		if ($campaignStarted){
+			$keyboard = new InlineKeyboard([
+				['text' => 'Follow our Twitter', 'url' => CatBot::app()->config->get('twitter_profile_to_follow_url')]
+			]);
+			$keyboard->setResizeKeyboard(true);
+			$keyboard->setOneTimeKeyboard(true);
+			
+			$text .= 'Follow us on Twitter, and retweet last post and paste link to tweet below:';
+		}
+		else{
+			$text = 'Oh shit, i think i have broken database.'
+						. PHP_EOL .
+						'Sorry, but i can not make you happy with tokens right now.'
+						. PHP_EOL .
+						'Try again later.';
+		}
+		
 		$data = [
 			'chat_id' => $chat_id,
-			'text'    => $text,
-			'reply_markup' => $keyboard,
+			'text'    => $text
 		];
 		
+		if (isset($keyboard)){
+			$data['reply_markup'] = $keyboard;
+		}
+	
 		return Request::sendMessage($data);
 	}
 }
